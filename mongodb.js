449e7f2be585cb7cd51b4fd3ec880c1e866406a8
yx <yangxing20107@qq.com>
2016-05-21 22:47:31 +0800

    ok

--- mongodb.js
+++ mongodb.js
@@ -6,18 +6,27 @@ mongoClient.connect(url, function(err, db) {   //都是在【回调函数】里�
   console.log("server Connected..");
   //do CRUD ==>> insert/delete/update/find 
   var collection = db.collection('myCollection');
+  
   collection.insertMany(
-  [{a:1}, {a:2}, {a:3}], function(err, result){}
+  [{a:1}, {a:2}, {a:3}], 
+  function(err, result){}
   );
+  
   collection.updateOne(
-  {a:2},{$set:{b:1}}, function(err, result){}
+  {a:2},{$set:{b:1}}, 
+  function(err, result){}
   );
+  
   collection.deleteOne(
-  {a:3}, function(err, result){}
+  {a:3}, 
+  function(err, result){}
   );
+  
   collection.find(
-  {},function(err, docs) {
+  {},
+  function(err, docs) {
   );
+  
   db.close();
 });
 
